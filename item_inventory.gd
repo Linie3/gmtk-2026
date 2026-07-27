ebcf215f8f15f5450c6552b0949f59c34c210ec6
Linus <lnia2004@web.de>
2026-07-27 11:09:46 +0200

    add item use-after-switch cooldown

--- item_inventory.gd
+++ item_inventory.gd
@@ -6,7 +6,7 @@ signal items_changed(new_items: Array[Item])
 signal active_item_changed(new_active_item: Item)
 
 @export
-var item_cooldown: Timer
+var item_switch_cooldown: Timer
 @export
 var items_container: Node2D
 @export
@@ -14,10 +14,10 @@ var resource_inventory: ResourceInventory
 var active_item: Item
 var active_item_cooldown_finished: bool = true
 var items: Array[Item] = []
+var cooldowns: Dictionary[StringName, SceneTreeTimer] = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	item_cooldown.timeout.connect(func(): active_item_cooldown_finished = true)
 	for child in items_container.get_children():
 		if (child is Item):
 			_add_item(child)
@@ -27,13 +27,9 @@ func _process(_delta: float) -> void:
 	if (active_item_movement != 0):
 		var current_item_index: int = items.find(active_item)
 		var new_item_index: int = clamp(current_item_index + active_item_movement, 0, items.size() - 1)
+		if current_item_index == new_item_index:
+			return
 		_set_active_item(items[new_item_index])
-
-func start_item_cooldown() -> void:
-	assert(active_item_cooldown_finished, "Cannot start item cooldown when one is already active")
-	active_item_cooldown_finished = false
-	item_cooldown.wait_time = active_item.cooldown
-	item_cooldown.start()
 
 func add_item(item: Item) -> void:
 	add_child(item)
@@ -56,12 +52,20 @@ func remove_item(item: Item) -> void:
 	items_changed.emit(items)
 
 func use_active_item(direction: Vector2) -> void:
-	if (active_item && active_item_cooldown_finished && resource_inventory.has_resources(active_item.usage_cost)):
-		resource_inventory.remove_resources(active_item.usage_cost)
-		start_item_cooldown()
-		active_item.use(direction)
+	if (
+		!active_item ||
+		(cooldowns.has(active_item.name) and cooldowns[active_item.name].get_time_left() > 0) ||
+		item_switch_cooldown.time_left > 0 ||
+		!resource_inventory.has_resources(active_item.usage_cost)
+	):
+		return
+	resource_inventory.remove_resources(active_item.usage_cost)
+	cooldowns[active_item.name] = get_tree().create_timer(active_item.cooldown)
+	active_item.use(direction)
 
 func _set_active_item(item: Item) -> void:
+	if active_item != item:
+		item_switch_cooldown.start()	
 	active_item = item
 	active_item_changed.emit(active_item)
 
